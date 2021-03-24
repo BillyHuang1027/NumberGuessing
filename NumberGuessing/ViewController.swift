ef04cd27a22dd1e278fa3eed1fa0e8bde389bf1f
Billy Huang <j28898601@gmail.com>
2021-03-24 20:45:13 +0800

    Initial Commit

---
+++ NumberGuessing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NumberGuessing
+//
+//  Created by 黃昌齊 on 2021/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
